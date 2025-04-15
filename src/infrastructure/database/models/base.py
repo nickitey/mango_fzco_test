7431b4dd46a021e8f2e4e840fa8027e103f1a61d
Nikita Savin <klaster26@mail.ru>
2025-04-16 01:00:26 +0300

    feat: реализовать метод конвертации модели в словарь

--- src/infrastructure/database/models/base.py
+++ src/infrastructure/database/models/base.py
@@ -3,4 +3,11 @@ from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(AsyncAttrs, DeclarativeBase):
-    pass
+    def to_dict(self):
+        data = {
+            column.name: getattr(self, column.name)
+            for column in self.__table__.columns
+        }
+        for rel in self.__mapper__.relationships:
+            data[rel.key] = getattr(self, rel.key)
+        return data
